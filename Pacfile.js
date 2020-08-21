819eceaa6bba2fdd0317c06fa5790bfdbb4ef1f0
quanganhbn <61528726+quanganhbn@users.noreply.github.com>
2020-08-21 20:30:57 +0700

    Create Pacfile.js

---
+++ Pacfile.js
@@ -0,0 +1,24 @@
+var DIRECT = "DIRECT";
+var PROXY = "PROXY 127.0.0.1:6074";
+var blacklist = {
+    "ocsp.apple.com":1,
+    "world-gen.g.aaplimg.com":1,
+    "mesu.apple.com":1,
+    "su.itunes.apple.com":1,
+    "gdmf.apple.com":1,
+};
+function FindProxyForURL(url, host) {
+  host = host.toLowerCase();
+  for (i = 0; i < 30; i++) {
+    if (blacklist[host]) {
+      return PROXY;
+    }
+    var index = host.indexOf(".");
+    if (index == -1) {
+      break;
+    } else {
+      host = host.substring(index + 1);
+    }
+  }
+  return DIRECT;
+}
